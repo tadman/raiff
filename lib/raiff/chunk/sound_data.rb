8d07aadd2f6ba1bcc086f7151365d0245d72e995
Scott Tadman <github@tadman.ca>
2010-12-06 14:23:14 -0500

    Normalize sample values

--- lib/raiff/chunk/sound_data.rb
+++ lib/raiff/chunk/sound_data.rb
@@ -19,6 +19,7 @@ class Raiff::Chunk::SoundData < Raiff::Chunk
     
     sample_count = (end_offset - file.offset) / common.bytes_per_sample
     bit_offset = common.bytes_per_sample * 8 - common.sample_size
+    value_offset = (1 << common.sample_size) - 1
     
     @samples = [ ]
     
@@ -28,14 +29,14 @@ class Raiff::Chunk::SoundData < Raiff::Chunk
       
       sample_count.times do
         if (sample = file.unpack(format))
-          @samples << sample.collect { |s| s.to_i(2) >> bit_offset }
+          @samples << sample.collect { |s| (s.to_i(2) >> bit_offset) - value_offset }
         end
       end
     else
       format = %w[ C n x N ][common.bytes_per_sample] * common.channels
       
       sample_count.times do
-        @samples << file.unpack(format).collect { |i| i >> bit_offset }
+        @samples << file.unpack(format).collect { |i| (i >> bit_offset) - value_offset }
       end
     end
   end
